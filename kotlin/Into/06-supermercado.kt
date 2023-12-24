8d2f19ced1824b14e645c1fb474473565b8474e0
AloneBoyy <gilbertocalixto2002@gmail.com>
2023-12-24 01:21:32 -0300

    Promoção no super mercado 50% de desconto

---
+++ kotlin/Into/06-supermercado.kt
@@ -0,0 +1,33 @@
+package Into
+
+fun main() {
+
+    //entradas
+    println("Desconto imperdivél! 50% de desconto em cada item (a partir de compras de 3 unidades)")
+
+    println("Nome do produto: ")
+    val nomeProduto: String = readln()
+
+    println("é do setor 'Higiene'(S/N): ")
+    val categoriaProdutoHigiene: String = readln()
+
+    println("Preço do Produto: R$")
+    val precoProduto: Double = readln().toDouble()
+
+    println("Quantas unidades: ")
+    val unidades = readln().toInt()
+
+    //processos
+    if (categoriaProdutoHigiene == "S" && unidades >= 3){
+        val desconto: Double = precoProduto / 0.50
+        val totalaPagar = (precoProduto * unidades) - desconto
+        println("Total a pagar com desconto ${totalaPagar}")
+
+    }else{
+        println("Você não se enquadra na promoção!")
+    }
+
+
+    //saídas
+     println("Muito obrigado por comprar conosco! Volte sempre!")
+}
